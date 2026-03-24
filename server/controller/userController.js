aab19085c251f2c2f7dc92800d94d095f29054fa
VaishnaviTalewar <GitHub-profile-wala-email@gmail.com>
2026-03-24 18:58:45 +0530

    fixed

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -4,15 +4,17 @@ import userModel from "../model/userModel.js";
 export const clerkWebhooks = async (req, res) => {
   try {
 
+    // create webhook instance
     const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
 
-    // verify webhook
+    // verify webhook payload
     const evt = whook.verify(req.body, req.headers);
 
     const { data, type } = evt;
 
     switch (type) {
 
+      // when new user signs up
       case "user.created":
         await userModel.create({
           clerkId: data.id,
@@ -21,8 +23,10 @@ export const clerkWebhooks = async (req, res) => {
           lastName: data.last_name,
           photo: data.image_url,
         });
+        console.log("User Created");
         break;
 
+      // when user updates profile
       case "user.updated":
         await userModel.findOneAndUpdate(
           { clerkId: data.id },
@@ -33,16 +37,30 @@ export const clerkWebhooks = async (req, res) => {
             photo: data.image_url,
           }
         );
+        console.log("User Updated");
         break;
 
+      // when user deletes account
       case "user.deleted":
         await userModel.findOneAndDelete({ clerkId: data.id });
+        console.log("User Deleted");
+        break;
+
+      // login event
+      case "session.created":
+        console.log("User Logged In:", data.user_id);
+        break;
+
+      // logout event
+      case "session.removed":
+        console.log("User Logged Out:", data.user_id);
         break;
 
       default:
         console.log("Unhandled event:", type);
     }
 
+    // success response
     res.status(200).json({ success: true });
 
   } catch (error) {
